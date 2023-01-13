777b3a0ac939b3e129fef6713dfdaa53b61d3b5c
darkninja1 <dahonzak@gmail.com>
2023-01-13 17:14:17 -0500

    ...

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from 'react'; //import React from 'react';
 import { Text, View, Image, StyleSheet, TouchableOpacity} from 'react-native';
 
 const styles = StyleSheet.create({
